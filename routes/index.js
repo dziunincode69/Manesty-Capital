367dc79e2fb8e94d6f7637935e599832220932cc
MuhammadWildanf <wildanf.daftar@gmail.com>
2023-08-03 13:07:25 +0700

    ...

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,9 @@ const Middlewares = require('../middlewares/auth.js');
 router.get("/", UserController.userHome)
 router.post("/login", UserController.userLogin)
 
+router.get("/register", UserController.registerForm)
+router.post("/register", UserController.regiterAccount)
+
 router.get("/dashboard", Middlewares.requireLogin, Middlewares.checkRoleUser, UserController.userDashboard)
 router.get("/logout", Middlewares.requireLogin, UserController.userLogout)
 
